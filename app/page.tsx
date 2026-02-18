6129bd5152488c003a2199544859146c3bc1cf85
Supun <infor.ssupun@gmail.com>
2026-02-18 13:16:54 +0530

    feat: Redirect the root path to the dashboard page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+'use client'
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  
+  const router = useRouter();
+  router.push("/features/dashboard");
   return (
     <div>
       Welcome to Super Green ERP!
