e1b6ba659155f216d022f8c59f3c2ed04c942548
Supun <infor.ssupun@gmail.com>
2026-05-25 10:56:05 +0530

    -up dep

--- lib/email.ts
+++ lib/email.ts
@@ -3,7 +3,7 @@ import nodemailer from "nodemailer";
 // Create transporter from environment SMTP config
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
-  port: Number(process.env.EMAIL_PORT ?? 587),
+  port: Number(process.env.EMAIL_PORT ?? 465),
   secure: process.env.EMAIL_PORT === "465", // true only for port 465
   auth: {
     user: process.env.EMAIL_USER,
